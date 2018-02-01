38d858480ff9b2bc470afa7aceeb8057d9dceedb
maxcrystal <hmi@ya.ru>
2018-02-01 08:36:14 +0300

    minor changes and tweeks

--- models/db1.py
+++ models/db1.py
@@ -14,6 +14,10 @@ request.now = request.utcnow
 if request.global_settings.web2py_version < "2.14.1":
     raise HTTP(500, "Requires web2py 2.14.1 or newer")
 
+# -------------------------------------------------------------------------
+# if SSL/HTTPS is properly configured and you want all HTTP requests to
+# be redirected to HTTPS, uncomment the line below:
+# -------------------------------------------------------------------------
 # request.requires_https()
 
 # application configuration using private/appconfig.ini
@@ -23,7 +27,9 @@ current.myconf = myconf
 myconf_env = myconf.get('environment.type')
 current.myconf_env = myconf_env
 
+# -----------------
 # set db connection
+# -----------------
 if not request.env.web2py_runtime_gae:
     # if NOT running on Google App Engine use SQLite or other DB
     db = DAL(myconf.get(f'{myconf_env}db.uri'),
@@ -41,21 +47,33 @@ else:
     # from google.appengine.api.memcache import Client
     # session.connect(request, response, db = MEMDB(Client()))
 
+# -------------------------------------------------------------------------
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
+# -------------------------------------------------------------------------
 response.generic_patterns = ['*'] if request.is_local else []
 
+# -------------------------------------------------------------------------
 # choose a style for forms
+# -------------------------------------------------------------------------
 response.formstyle = myconf.get('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
 response.form_label_separator = myconf.get('forms.separator') or ''
 
-# optimize handling of static files
+# -------------------------------------------------------------------------
+# (optional) optimize handling of static files
+# -------------------------------------------------------------------------
 # response.optimize_css = 'concat,minify,inline'
 # response.optimize_js = 'concat,minify,inline'
 
-# static assets folder versioning
+# -------------------------------------------------------------------------
+# (optional) static assets folder versioning
+# -------------------------------------------------------------------------
 # response.static_version = '0.0.0'
 
+
+# -------------------------------------------------------------------------
+# create all tables needed by auth, maybe add a list of extra fields
+# -------------------------------------------------------------------------
 from gluon.tools import Auth, Service, PluginManager
 
 auth = Auth(db, host_names=myconf.get(f'{myconf_env}host.name'))
@@ -80,7 +98,9 @@ auth.define_tables(username=True, signature=True)
 # add auth formatting, validation, and representation
 db.auth_user._format = '%(first_name)s %(last_name)s (%(id)s)'  # defaults to '%(username)'
 
+# -------------------------------------------------------------------------
 # configure email
+# -------------------------------------------------------------------------
 mail = auth.settings.mailer
 mail.settings.server = myconf.get(f'{myconf_env}smtp.server')
 mail.settings.sender = myconf.get(f'{myconf_env}smtp.sender')
@@ -88,7 +108,9 @@ mail.settings.login = myconf.get(f'{myconf_env}smtp.login')
 mail.settings.tls = myconf.get(f'{myconf_env}smtp.tls') or False
 mail.settings.ssl = myconf.get(f'{myconf_env}smtp.ssl') or False
 
+# -------------------------------------------------------------------------
 # configure auth policy
+# -------------------------------------------------------------------------
 auth.settings.actions_disabled.append('register')
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
@@ -96,10 +118,30 @@ auth.settings.reset_password_requires_verification = True
 auth.settings.create_user_groups = False  # defaults to True
 auth.settings.expiration = 60 * 60 * 24  # seconds
 
-# after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+# -------------------------------------------------------------------------
+# read more at http://dev.w3.org/html5/markup/meta.name.html
+# -------------------------------------------------------------------------
+response.meta.name = myconf.get('app.name')
+response.meta.author = myconf.get('app.author')
+response.meta.description = myconf.get('app.description')
+response.meta.keywords = myconf.get('app.keywords')
+response.meta.generator = myconf.get('app.generator')
 
+# -------------------------------------------------------------------------
+# your http://google.com/analytics id
+# -------------------------------------------------------------------------
+response.google_analytics_id = myconf.get('google.analytics_id')
+
+# -------------------------------------------------------------------------
+# maybe use the scheduler
+# -------------------------------------------------------------------------
+if myconf.get('scheduler.enabled'):
+    from gluon.scheduler import Scheduler
+    scheduler = Scheduler(db, heartbeat=myconf.get('heartbeat'))
+
+# -------------------------------------------------------------------------
 # add db, auth, mail to current for access from modules
+# -------------------------------------------------------------------------
 current.db = db
 current.auth = auth
 current.mail = mail
---
+++ models/z_after_all.py
@@ -0,0 +1,4 @@
+# -------------------------------------------------------------------------
+# after defining tables, uncomment below to enable auditing
+# -------------------------------------------------------------------------
+# auth.enable_record_versioning(db)
